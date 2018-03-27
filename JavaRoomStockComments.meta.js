933451267d7efc25590c65dd752f7e9631c28f91
geisterfurz007 <geisterfurz007@gmail.com>
2018-03-27 14:06:30 +0200

    Added meta file for autoupdate

---
+++ JavaRoomStockComments.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Stock Comments
+// @namespace    https://github.com/geisterfurz007
+// @version      0.2.3
+// @description  Easily send stock messages with the click of a button
+// @author       geisterfurz007
+// @match        https://chat.stackoverflow.com/rooms/139/*
+// @grant        GM_addStyle
+// @grant        GM.xmlHttpRequest
+// @grant        GM_xmlhttpRequest
+// @downloadURL https://github.com/geisterfurz007/JavaRoomStockComments/raw/master/JavaRoomStockComments.user.js
+// @updateURL https://github.com/geisterfurz007/JavaRoomStockComments/raw/master/JavaRoomStockComments.meta.js
+// ==/UserScript==
